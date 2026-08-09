332b2be8087f2626d3616001047e84a25ac50737
greyhair-atx <greyhair-atx@users.noreply.github.com>
2026-08-08 23:16:53 -0500

    Activate host when embedded terminal is clicked

--- SuperPutty/Utils/NativeMethods.cs
+++ SuperPutty/Utils/NativeMethods.cs
@@ -1329,7 +1329,7 @@ namespace SuperPutty.Utils
 
         public delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
 
-        [DllImport("user32.dll")]
+        [DllImport("user32.dll", SetLastError = true)]
         public static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess, uint idThread, uint dwFlags);
 
         [DllImport("user32.dll")]
