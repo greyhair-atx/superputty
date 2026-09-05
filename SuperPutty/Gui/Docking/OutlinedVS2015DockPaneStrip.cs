d634789774a07e4a22069b3dd5b4303c33310c44
greyhair-atx <greyhair-atx@users.noreply.github.com>
2026-09-04 21:07:42 -0500

    Tighten document tab text padding

--- SuperPutty/Gui/Docking/OutlinedVS2015DockPaneStrip.cs
+++ SuperPutty/Gui/Docking/OutlinedVS2015DockPaneStrip.cs
@@ -116,11 +116,11 @@ namespace SuperPutty.Gui.Docking
         private const int _DocumentTabGapLeft = 0;//3;
         private const int _DocumentTabGapRight = 0;//3;
         private const int _DocumentIconGapBottom = 2;//2;
-        private const int _DocumentIconGapLeft = 8;
+        private const int _DocumentIconGapLeft = 7;
         private const int _DocumentIconGapRight = 0;
         private const int _DocumentIconHeight = 16;
         private const int _DocumentIconWidth = 16;
-        private const int _DocumentTextGapRight = 6;
+        private const int _DocumentTextGapRight = 4;
 
         #endregion
 
